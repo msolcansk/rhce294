b8858d983dac51d26d104fecea9d8c08a0dd489c
msolcansk <solcansky.martin@gmail.com>
2020-10-26 21:17:51 +0100

    added ansible package/installation

--- scripts/allnodes.sh
+++ scripts/allnodes.sh
@@ -3,3 +3,5 @@ useradd ansible
 echo "ansible" | passwd --stdin ansible
 echo "ansible ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/ansible
 alternatives --set python /usr/bin/python3
+yum install -y epel-release
+yum install -y ansible
