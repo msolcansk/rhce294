9bf46a65d85018638e4cade0669318cc0ef1ef2d
msolcansk <solcansky.martin@gmail.com>
2020-10-28 08:41:39 +0100

    add sshd passwordauth yes

--- scripts/allnodes.sh
+++ scripts/allnodes.sh
@@ -5,3 +5,7 @@ echo "ansible ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/ansible
 alternatives --set python /usr/bin/python3
 yum install -y epel-release
 yum install -y ansible
+
+#SSH CONFIG
+sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
+systemctl restart sshd.service
